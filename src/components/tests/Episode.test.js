01bff8ea50b840c7e1f9525c8291ee437568f3c3
Your Name <mari@Maribels-MacBook-Pro.local>
2022-02-23 20:41:45 -0600

    solved for episode & show

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,8 +3,41 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
 
-test("renders without error", () => { });
+const testData = {
+    id: 1,
+    image: '',
+    name: 'My favorite episode',
+    season: 1,
+    number: 4,
+    summary: 'We need season 5 already, sheesh!',
+    runtime: '50 minutes' 
+}
+const imageData = {
+    id: 1,
+    image: null,
+    name: 'My favorite episode',
+    season: 1,
+    number: 4,
+    summary: 'We need season 5 already, sheesh!',
+    runtime: '50 minutes' 
+}
 
-test("renders the summary test passed as prop", () => { });
+test("renders without error", () => { 
+    render(<Episode episode={['Stranger Example']}/>);
+ });
 
-test("renders default image when image is not defined", () => { });
+test("renders the summary test passed as prop", () => { 
+    render(<Episode episode={testData}/>);
+
+    const summary = screen.queryByText(/need/i);
+    expect(summary).toBeVisible();
+    expect(summary).toBeInTheDocument();
+    expect(summary).toHaveTextContent('season');
+});
+
+test("renders default image when image is not defined", () => { 
+    render(<Episode episode={imageData}/>);
+
+    const image = screen.queryByAltText('https://i.ibb.co/2FsfXqM/stranger-things.png');
+    expect(image).toBeInTheDocument();
+});
--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -1,14 +1,70 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Show from './../Show';
+import userEvent from '@testing-library/user-event'
 
-test('renders without errors', () => { });
+const showData = {
+    name: 'Hong Kong Fui',
+    summary: 'cartoon karate',
+    seasons: [
+        {
+            id: 1,
+            name: 'season 1',
+            episodes: []
+        },
+        {
+            id: 2,
+            name: 'season 2',
+            episodes: []
+        }
+    ]
+}
 
-test('renders Loading component when prop show is null', () => { });
+test('renders without errors', () => { 
+    render(<Show show={showData} selectedSeason= 'none'/>)
+});
 
-test('renders same number of options seasons are passed in', () => { });
+test('renders Loading component when prop show is null', () => {
+    render(<Show show={null}/>);
 
-test('handleSelect is called when an season is selected', () => { });
+    const loading = screen.queryByTestId('loading-container');
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => { });
+    expect(loading).toBeTruthy();
+    expect(loading).toBeInTheDocument();
+    expect(loading).toBeVisible();
+
+ });
+
+test('renders same number of options seasons are passed in', () => { 
+    render(<Show show = {showData} selectedSeason = 'none'/>)
+
+    const seasonOptions = screen.queryAllByTestId('season-option');
+
+    expect(seasonOptions).toHaveLength(2);
+});
+
+test('handleSelect is called when an season is selected', () => { 
+    const handleSelect = jest.fn();
+
+    render(<Show show={showData} selectedSeason={'none'} handleSelect={handleSelect}/>)
+
+    const select = screen.getByLabelText(/select a season/i);
+    userEvent.selectOptions(select, ['1']);
+
+    expect (handleSelect).toBeCalled();
+});
+
+test('component renders when no seasons are selected and when rerenders with a season passed in', () => { 
+    const { rerender } = render(<Show show={showData} selectedSeason={'none'}/>);
+
+    let episodes = screen.queryByTestId('episodes-container');
+
+    expect(episodes).not.toBeInTheDocument();
+
+    rerender(<Show show={showData} selectedSeason={1}/>);
+
+    episodes = screen.queryByTestId('episodes-container');
+
+    expect(episodes).toBeInTheDocument();
+});
